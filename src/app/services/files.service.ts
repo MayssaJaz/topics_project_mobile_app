9d1148a392e5084d271fac7a4da5ca89d88e0185
Mayssa Jaziri <jazirimayssa20@gmail.com>
2025-03-24 00:29:32 +0100

    fix-small-bug

--- src/app/services/files.service.ts
+++ src/app/services/files.service.ts
@@ -1,55 +1,34 @@
 import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GoogleStorageService {
-  private backendUrl = 'http://localhost:3000/generate-signed-url';
-
+  private apiUrl = 'http://localhost:3000/upload';
   constructor() {}
 
-  async uploadFile(file: File): Promise<string> {
+  async uploadFile(file: File): Promise<any> {
     try {
-      const uploadUrl = `https://storage.googleapis.com/upload/storage/v1/b//o?uploadType=media&name=${file.name}`;
-      const accessToken = environment.accessToken;
-      const response = await fetch(uploadUrl, {
-        method: 'POST',
-        headers: {
-          'Content-Type': file.type,
-          Authorization: `Bearer ${accessToken}`,
-        },
-        body: file,
-      });
+      const formData = new FormData();
+      formData.append('file', file, file.name);
 
-      if (!response.ok) {
-        console.log(await response.text());
-        throw new Error('Upload failed');
-      }
-
-      const data = await response.json();
-      const fileName = data.name;
-
-      const signedUrlResponse = await fetch(
-        `${this.backendUrl}?fileName=${fileName}`,
+      const response = await fetch(
+        `${this.apiUrl}?fileName=${encodeURIComponent(file.name)}`,
         {
-          method: 'GET',
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
+          method: 'POST',
+          body: formData,
         }
       );
 
-      if (!signedUrlResponse.ok) {
-        console.error(await signedUrlResponse.text());
-        throw new Error('Error generating signed URL');
+      if (!response.ok) {
+        const errorMessage = await response.text();
+        throw new Error(`File upload failed: ${errorMessage}`);
       }
 
-      const signedUrlData = await signedUrlResponse.json();
-      return signedUrlData.signedUrl;
+      const result = await response.json();
+      return result.publicUrl;
     } catch (error) {
-      console.error('Upload error:', error);
-      throw error;
+      throw new Error('File upload failed!');
     }
   }
 }
